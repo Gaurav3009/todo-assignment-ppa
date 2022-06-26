08b06838d21e2aeabd57ccc8c47147cf930a9b55
Gaurav3009 <onlyprofessionalmails@gmail.com>
2022-06-26 23:55:36 +0530

    react assignment

--- todoapp/src/App.js
+++ todoapp/src/App.js
@@ -1,3 +1,4 @@
+
 import React, { Component } from 'react';
 import './App.css';
 
@@ -56,41 +57,42 @@ class App extends Component{
   render(){
     return(
       <div>
-        <h1>Todo List</h1>
+        <h1 id = "mainHead">Todo List</h1>
         <div id={"addDiv"}>
-          <h4>Add Todo</h4>
-          <input id={"textInput"} type={"text"} placeholder={"Add new item"} 
+          <h4 style={{display : 'inline', marginRight : '1vw', fontFamily: '"Joan", serif', fontSize : '2rem'}}>What task you want to add?</h4>
+          <input style = {{borderRadius : '10px'}} id={"textInput"} type={"text"} placeholder={"Add new item"} 
           value={this.state.itemText} name={"itemText"} onChange = { (event) =>{
             this.handleChange(event);
           } } 
           required />
 
-          <br/>
-          <button id={"addBtn"} type={"submit"} onClick={() => {
+          {/* <br/> */}
+          <button style = {{marginLeft : '1vw'}} id={"addBtn"} type={"submit"} onClick={() => {
             this.handleAddItem();
           }}>
           Submit </button>
           </div>
 
-          <div id={"listDiv"}>
-            <h4> Tasks list </h4>
-            {
-              this.state.allItems.map((item) => (
-                <div className={"listItems"} key={item.id}>
-                  {item.isCompleted ? <div className={"taskName isDone"}><strike>{item.todoDes}</strike></div> : <div className={"taskName"}>{item.todoDes}</div>}
-                  <span className={"button-span"}> 
-                  <button className={"btn-done"} onClick={() => {
-                      this.handleTaskDone(item);
-                      }
-                    }> ✔️ </button>
-                    <button className={"btn-remove"} onClick={() => {
-                      this.handleItemDelete(item);
-                      }
-                    }> ❌ </button> 
-                  </span>
-                </div>
-              ))
-            }
+          <div style = {{height : '50vh', paddingTop : '1px'}}>
+            <div style = {{background : '#F9CEEE', }} id={"listDiv"}>
+              {
+                this.state.allItems.map((item) => (
+                  <div  className={"listItems"} key={item.id}>
+                    {item.isCompleted ? <div className={"taskName isDone"}><strike>{item.todoDes}</strike></div> : <div className={"taskName"}>{item.todoDes}</div>}
+                    <span className={"button-span"}> 
+                    <button className={"btn-done"} onClick={() => {
+                        this.handleTaskDone(item);
+                        }
+                      }> Done </button>
+                      <button className={"btn-remove"} onClick={() => {
+                        this.handleItemDelete(item);
+                        }
+                      }> Delete </button> 
+                    </span>
+                  </div>
+                ))
+              }
+            </div>
           </div>
       </div>
     );
